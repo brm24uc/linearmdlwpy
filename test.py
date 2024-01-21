84e56cc1253b650fe9c41c3e67e6379e0fb8e7d0
Byron Marsh <brm1811@gmail.com>
2024-01-21 17:11:37 -0500

    adding notes notebook

--- test.py
+++ test.py
@@ -1,29 +1,26 @@
 # https://docs.github.com/en/migrations/importing-source-code/using-the-command-line-to-import-source-code/adding-locally-hosted-code-to-github
 
-# to create a repo from locally developed code
-
-# creates repo locally 
-# - navigate to directory 
-# - git init .
-# - git add .
-# - git commit -a -m "First commit."
-
 # remote repos
 # - note there are ssh urls and https urls 
 # - currently set up to run ssh urls 
-# see # https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories#cloning-with-https-urls
 
 # remote repos general
 # https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories
+# https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories#cloning-with-https-urls
 
-# push repo to github
+# to clone repo 
+# -------------
+# - create repo on gitdashboard
+# - git clone ssh or https url 
 
-# https 
-# git remote add origin https://github.com/brm24uc/linearmodelswpython.git
+# to create a repo from locally developed code
+# -------------------------------------------
+# Notes
+# - dont do this - create a repo from dashboard and clone to directory of choice
 
-# ssh
-# git@github.com:brm24uc/linearmodelswpython.git
-# git remote set-url --push origin 'git@github.com:User/Project.git'
+
+
+
 
 
 
