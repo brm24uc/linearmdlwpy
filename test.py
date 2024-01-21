f3381bec57dcbb2f4b06332a8ffc229ae36e5c66
Byron Marsh <brm1811@gmail.com>
2024-01-21 12:41:09 -0500

    First commit.

---
+++ test.py
@@ -0,0 +1,30 @@
+# https://docs.github.com/en/migrations/importing-source-code/using-the-command-line-to-import-source-code/adding-locally-hosted-code-to-github
+
+# to create a repo from locally developed code
+
+# creates repo locally 
+# - navigate to directory 
+# - git init .
+# - git add .
+# - git commit -a -m "First commit."
+
+# remote repos
+# - note there are ssh urls and https urls 
+# - currently set up to run ssh urls 
+# see # https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories#cloning-with-https-urls
+
+# remote repos general
+# https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories
+
+# push repo to github
+
+# https 
+# git remote add origin https://github.com/brm24uc/linearmodelswpython.git
+
+# ssh
+# git@github.com:brm24uc/linearmodelswpython.git
+# git remote set-url --push origin 'git@github.com:User/Project.git'
+
+
+
+
